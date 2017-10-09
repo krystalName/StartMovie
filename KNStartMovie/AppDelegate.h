a6a33c4e9f241ec57ae1dc254a4063e1b9a9c8a2
Leessang <992275294@qq.com>
2017-10-09 11:05:03 +0800

    Initial Commit

---
+++ KNStartMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNStartMovie
+//
+//  Created by 刘凡 on 2017/10/9.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNStartMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNStartMovie
+//
+//  Created by 刘凡 on 2017/10/9.
+//  Copyright © 2017年 KrystalName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
